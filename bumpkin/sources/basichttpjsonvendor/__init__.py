9f1f728289f137d1f8b48775b1780126ae516dab
lucasew <lucas59356@gmail.com>
2023-03-05 11:45:55 -0300

    lintwork
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bumpkin/sources/basichttpjsonvendor/__init__.py
+++ bumpkin/sources/basichttpjsonvendor/__init__.py
@@ -43,6 +43,6 @@ class BasicHTTPJSONVendorSource(BaseSource):
 
     @classmethod
     def argparse(cls, parser):
-        parser.description = "Like basichttp but passes through the JSON content as the return value instead of instructing how to download it" # noqa: E501
+        parser.description = "Like basichttp but passes through the JSON content as the return value instead of instructing how to download it"  # noqa: E501
         parser.add_argument("url", type=str)
         return parser
