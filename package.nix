5f4d016e366d8c5a1b88e9e8e21cf3b1b7a8b6e3
lucasew <lucas59356@gmail.com>
2025-09-11 21:24:05 -0300

    deal with nix stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- package.nix
+++ package.nix
@@ -1,10 +1,13 @@
-{ buildPythonPackage, pytest, callPackage }:
+{ buildPythonPackage, pytest, callPackage, hatchling }:
 let
   bumpkin = buildPythonPackage {
     pname = "bumpkin";
     version = builtins.readFile ./bumpkin/VERSION;
+    format = "pyproject";
 
     src = ./.;
+
+    buildInputs = [ hatchling ];
 
     checkInputs = [ pytest ];
 
